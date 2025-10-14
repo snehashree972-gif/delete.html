266a1ac42a81d497d9de5988a5b64e41cb0414f1
Sneha shree <snehashree792@.com>
2025-10-14 11:54:50 +0530

    add swigg clone

--- Swiggiclone/Frontend/src/pages/Login.jsx
+++ Swiggiclone/Frontend/src/pages/Login.jsx
@@ -4,6 +4,17 @@ const Login = () => {
   return (
     <>
     <div> Login</div>
+    <div>
+      <form>
+        <lable>Name</lable>
+            <input type="text" placeholder="Name" require="true" value={name} onChange={e.target.value} />
+            <lable>Phone Number</lable>
+            <input type="Number" placeholder="Phone Number" require="true" value={phonenumber} onChange={e.target.value} />
+            <lable>Email Address</lable>
+            <input type="Email Address" placeholder="Email Address" require="true" value={emailaddress} onChange={e.target.value}/>
+            <button type="submit">Submit</button>
+      </form>
+    </div>
 
 </>
   )
--- Swiggiclone/Frontend/src/pages/Signup.jsx
+++ Swiggiclone/Frontend/src/pages/Signup.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Signup = () => {
+  const [name , setName] = useState("")
   return (
-    <>
+    <> 
     <div> Signup</div>
     <div>
         <form>
             <lable>Name</lable>
-            <input type="text" placeholder="Name" require="true" />
+            <input type="text" placeholder="Name" require="true" value={name} onChange={e.target.value}/>
             <lable>Phone Number</lable>
-            <input type="number" placeholder="Phone Number" require="true" />
+            <input type="number" placeholder="Phone Number" require="true" value={Number} onChange={e.target.value} />
             <label>Email Address</label>
-            <input type="email address" placeholder="Email Address" require="true" />
+            <input type="email address" placeholder="Email Address" require="true" value={emailaddress} onChange={e.target.value} />
             <lable>Password</lable>
-            <input type="password" placeholder="Password" require="true" />
+            <input type="password" placeholder="Password" require="true" value={password} onChange={e.target.value} />
             <button type="submit">Submit</button>
 
         </form>
     </div>
     </>
-    
+
   )
 }
 
